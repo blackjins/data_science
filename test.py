656fd80dfbb2f1580115affe4213cc0867d2f9db
유영서 <dndudtjqw123@naver.com>
2025-03-12 08:58:56 +0900

    test

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
-import pandas as pd
-data_url = 'https://raw.githubusercontent.com/codingalzi/DataSci/refs/heads/master/data/'
-#dataset 기본 주소
-sports_df = pd.read_csv(data_url + 'ch01_sport_test.csv')
-print(sports_df.loc[1]) 
+import numpy as np
+arr1 = np.array([1, 2, 3, 4])
+arr2 = [2, 3]
+print(type(np.mean(arr2)))
